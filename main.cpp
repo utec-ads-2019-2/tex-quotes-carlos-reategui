f19c4a2a19c37bd3149ba40f33f7f1053329d19a
carlos-reategui <carlos.reategui@utec.edu.pe>
2019-08-24 11:22:24 -0500

    accepted

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-using namespace std;
-
-int main(int argc, char *argv[]) {
+int main() {
+    char character;
+    bool firstQuote = false;
+    while(scanf("%c", &character) != EOF) {
+        if(character == '"') {
+            if(!firstQuote) {
+                std::cout << "``";
+                firstQuote = true;
+            }
+            else {
+                std::cout << "''";
+                firstQuote = false;
+            }
+        }
+        else
+            std::cout << character;
+    }
     return 0;
 }
